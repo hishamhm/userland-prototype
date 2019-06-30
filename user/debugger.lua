3f539c24427f207dc8bd82cde2f46be5e97ff57a
Hisham Muhammad <hisham@gobolinux.org>
2019-06-30 12:39:15 -0300

    fix debugger eval window location

--- user/debugger.lua
+++ user/debugger.lua
@@ -7,8 +7,9 @@ function debugger.init(ui_)
 end
 
 function debugger.new(self)
-   local history = self.parent.parent
-   history:add_child(ui.vbox({ name = "window", scroll_by = 21, min_w = 492, max_w = 492, max_h = 400, spacing = 4, fill = 0x222222, border = 0x00ffff }, {
+   local window = self.parent.parent
+   local history = window.parent
+   window:add_child(ui.vbox({ name = "window", scroll_by = 21, min_w = 492, max_w = 492, max_h = 400, spacing = 4, fill = 0x222222, border = 0x00ffff }, {
       ui.tree(_G)
    }))
    history.data.add_prompt(history, "?", " ? ")
